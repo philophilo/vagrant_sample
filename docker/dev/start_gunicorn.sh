a456c4ed05cabb976e93e51d933630c342a6a6a3
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-06-19 11:51:28 +0300

    run gunicorn on port 8000

--- docker/dev/start_gunicorn.sh
+++ docker/dev/start_gunicorn.sh
@@ -2,4 +2,4 @@
 cd /app
 export $(cat .env | xargs)
 . venv/bin/activate
-gunicorn manage:app --worker-class gevent --worker-connections 1000 --timeout 30 -b 0.0.0.0:80 --reload --log-syslog
+gunicorn manage:app --worker-class gevent --worker-connections 1000 --timeout 30 -b 0.0.0.0:8000 --reload --log-syslog
